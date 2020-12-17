a69322c65d64cb589785ff3526f30204488d8783
Bysiv01 <45607527+Bysiv01@users.noreply.github.com>
2020-12-17 20:09:25 +0100

    poprawa

---
+++ PIII_kol/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PIII_kol
+{
+    class Komentarz
+    {
+        public string Autor;
+        public string KomentowanyPost;
+        public string Tresc;
+        public string[] Tagi;
+        public DateTime CzasStworzenia;
+
+        public Komentarz(string autor, string komentowany, string tresc, string[] tagi, DateTime czasStworzenia)
+        {
+            if (string.IsNullOrWhiteSpace(komentowany) && string.IsNullOrWhiteSpace(autor))
+                throw new ArgumentException("Autor i Komentowany post nie mogą być puste");
+            else
+            {
+                Autor = autor;
+                KomentowanyPost = komentowany;
+            }
+            Tresc = tresc;
+            //if (string.IsNullOrWhiteSpace(tagi) throw new AggregateException("Tagi nie mogą być puste");
+            Tagi = tagi;
+           
+            DateTime data = new DateTime(2020, 1, 1);
+            if (czasStworzenia < data) CzasStworzenia = czasStworzenia;
+            else throw new ArgumentException("Data utworzenia nie może być starsza niż 01.01.2020r");
+        }
+    }
+        
+}
+
--- PIII_kol/Program.cs
+++ PIII_kol/Program.cs
@@ -6,24 +6,7 @@ using System.Text;
 
 namespace PIII_kol
 {
-    class Komentarz
-    {
-        public string Autor;
-        public string KomentowanyPost;
-        public string Tresc;
-        public int[] Tagi;
-        public string CzasStworzenia;
 
-        public Komentarz(string autor, string komentowany, string tresc, int[] tagi, string czas)
-        { 
-            Autor = autor;
-            KomentowanyPost = komentowany;
-            Tresc = tresc;
-            Tagi = tagi;
-            CzasStworzenia = czas;
-        }
-        
-    }
 
     class Program
     {
@@ -35,8 +18,10 @@ namespace PIII_kol
             Klimatyzacja = 2,
             Nawigacja = 3,
             Radio = 4,
-            CD = 5
+            CD = 5,
+            wyjscie = 6
         };
+
         static void Main(string[] args)
         {
             int minuty;
@@ -57,16 +42,54 @@ namespace PIII_kol
                 Console.WriteLine("Kolejny trening? tak/nie");
             }
             while ((Console.ReadLine() == "tak"));
-            Console.WriteLine($"lacznie trenowales {suma_minut} minut i {suma_Sekund} sekund podczas {trening} treningow");
+            Console.WriteLine($"lacznie trenowales {suma_minut} minut(y) i {suma_Sekund} sekund(y) podczas {trening} treningow");
 
-            Console.WriteLine("Dostępne Opcje modułu komfortu: ");
-            for (int val = 0; val <= 5; val++)
-                Console.WriteLine("{0,4} - {1:G}", val, (Kontrola)val);
+            static void wypisz()
+            {
+                Console.WriteLine("Dostępne Opcje modułu komfortu: ");
+                for (int value = 0; value <= 5; value++)
+                    Console.WriteLine("{0,4} - {1:G}", value, (Kontrola)value);
+            }
 
-            //nie wiem w jaki sposób dać użytkownikowi możliwość wyboru 
+            do
+            { 
+            string value;
+            int NrOpcji;
+            wypisz();
+            value = Console.ReadLine();
+            NrOpcji = Convert.ToInt32(value);
+                switch (NrOpcji)
+                {
+                    case 0:
+                        Console.WriteLine($"{Kontrola.Chłodzenie} 1");
+                        break;
+                    case 1:
+                        Console.WriteLine($"{Kontrola.Nagrzewanie} 1");
+                        break;
+                    case 2:
+                        Console.WriteLine($"{Kontrola.Klimatyzacja} 1");
+                        break;
+                    case 3:
+                        Console.WriteLine($"{Kontrola.Nawigacja} 1");
+                        break;
+                    case 4:
+                        Console.WriteLine($"{Kontrola.Radio} 1");
+                        break;
+                    case 5:
+                        Console.WriteLine($"{Kontrola.CD} 1");
+                        break;
+                }
+                Console.WriteLine("x aby kontynuowac");
+            }
+            while ((Console.ReadLine() == "x"));
 
-            Komentarz a = new Komentarz("uzytkownik1", "post1", "dluga wypowiedz", [1] ,"data");
-
+            Komentarz komentarz = new Komentarz("uzytkownik1", "post1", "wypowiedz", new[] { "tag" }, new DateTime(2019, 12 ,12));
+    
+            Console.WriteLine(komentarz.Autor);
+            Console.WriteLine(komentarz.KomentowanyPost);
+            Console.WriteLine(komentarz.Tresc);
+            Console.WriteLine(komentarz.Tagi);
+            Console.WriteLine(komentarz.CzasStworzenia);
         }
     }
 }
